8ef1fc539061b1ffb7262bb4df300de3d72f8494
Usman S. <ui.sabuwala@gmail.com>
2025-11-08 10:17:33 +0530

    add react query

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,38 +1,45 @@
-import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 
 import appCss from "../styles.css?url";
 import { getSessionFn } from "@/server/auth";
+import type { QueryClient } from "@tanstack/react-query";
 
-export const Route = createRootRoute({
-  beforeLoad: async () => {
-    const session = await getSessionFn();
-    return { session };
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
+  {
+    beforeLoad: async () => {
+      const session = await getSessionFn();
+      return { session };
+    },
+    head: () => ({
+      meta: [
+        {
+          charSet: "utf-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          title: "TanStack Start Starter",
+        },
+      ],
+      links: [
+        {
+          rel: "stylesheet",
+          href: appCss,
+        },
+      ],
+    }),
+
+    shellComponent: RootDocument,
   },
-  head: () => ({
-    meta: [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      {
-        title: "TanStack Start Starter",
-      },
-    ],
-    links: [
-      {
-        rel: "stylesheet",
-        href: appCss,
-      },
-    ],
-  }),
-
-  shellComponent: RootDocument,
-});
+);
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
